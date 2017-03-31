190b3a982db17eaced1682dac686add9e641e6ee
moumoux1 <eliedl1596@gmail.com>
2017-03-31 10:46:23 -0400

    Update core, lorentz

--- projet_core.py
+++ projet_core.py
@@ -2,61 +2,62 @@ import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-
-def f(t, y, sigma, rho, beta):
-    return [sigma*(y[1] - y[0]), rho*y[0] - y[1] - y[0]*y[2], y[0]*y[1] - beta*y[2]]
-
-def pend(l, t, sigma, rho, beta):
-     x, y, z  = l
-     dldt = [sigma*(y - x) , rho*x - y - x*z, x*y - beta*z]
-     return dldt
-
-sigma, rho, beta = 10, 28, 8/3
-t = np.linspace(1, 100, 10001)
-y0 = [0.5, 0.5, 0.5]
-y1 = [1, 0.5, 0.5]
-
-sol = odeint(pend, y0, t, args=(sigma, rho, beta))
-sol_1 = odeint(pend, y1, t, args=(sigma, rho, beta))
-
-sol_copy = np.copy(sol[:, 0])
-sol_1_copy = np.copy(sol_1[:, 0])
-print(np.size(sol[:, 0]) == np.size(sol_copy))
-x_n = np.delete(sol[:, 0], -1)
-x_nn = np.delete(sol_copy, 0)
-
-
-x_n_ = np.delete(sol_1[:, 0], -1)
-x_nn_ = np.delete(sol_1_copy, 0)
-
-print(len(x_n), len(x_nn))
-
-plt.plot(x_n, x_nn)
-plt.plot(x_n_, x_nn_)
-plt.show()
-
-plt.plot(t, sol[:, 0], 'b', label='x_0')
-plt.plot(t, sol_1[:, 0], 'g', label='x_1')
-plt.legend(loc='best')
-plt.xlabel('t')
-plt.grid()
-plt.show()
-
-plt.plot(x_n, x_n_)
-plt.show()
-
-
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 
-mpl.rcParams['legend.fontsize'] = 10
+class Core:
+    def __init__(self):
+        self.attractor = ''
+        self.coordinates = []
+        self.params = np.array([])
+        self.time_series = np.array([0, 0, 0])
+        self.t = np.array([])
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-x = sol[:, 0]
-y = sol[:, 1]
-z = sol[:, 2]
-ax.plot(x, y, z, label='Lorentz attractor')
-ax.legend()
+    @staticmethod
+    def lorentz(l, t, sigma ,rho, beta):
 
-plt.show()
+        x, y, z  = l
+
+        dldt = [sigma*(y - x) , rho*x - y - x*z, x*y - beta*z]
+        return dldt
+
+    def solve_lorentz(self):
+        for i in range(np.shape(self.coordinates)[0]):
+            sol = odeint(self.lorentz, self.coordinates[i, :], self.t, args=(self.params[0, 0], self.params[0, 1], self.params[0, 2]))
+
+            if i == 0:
+                self.time_series =  sol
+            else:
+                self.time_series = np.hstack((self.time_series, sol))
+
+            self.r_n = np.delete(self.time_series, -1, 0)
+            self.r_nn = np.delete(self.time_series, 0, 0)
+
+
+
+if __name__ == '__main__':
+    core_1 = Core()
+    core_1.coordinates = np.array([[1, 1, 1], [1+1e-5, 1+1e-5, 1+1e-5]])
+    core_1.attractor = 'Lorentz'
+    core_1.params = np.array([[10, 28, 8/3]])
+    core_1.t = np.linspace(1, 100, 10001)
+    core_1.solve_lorentz()
+
+    mpl.rcParams['legend.fontsize'] = 10
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    #x = core_1.time_series[:, 0]
+    #y = core_1.time_series[:, 1]
+    #z = core_1.time_series[:, 2]
+
+    x = core_1.time_series[:, 0] - core_1.time_series[:, 3]
+    y = core_1.time_series[:, 1] - core_1.time_series[:, 4]
+    z = core_1.time_series[:, 2] - core_1.time_series[:, 5]
+
+    r = np.sqrt(x**2 + y**2 + z**2)
+    ax.plot(x, y, z, label='Lorentz attractor', marker = '.', markersize = 2, ls='none')
+    ax.legend()
+    plt.show()
+
+    plt.plot(core_1.t, r)
+    plt.show()
